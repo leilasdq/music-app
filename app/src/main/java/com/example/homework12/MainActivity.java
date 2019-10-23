d453723a788a8361bdddb084c38f5c24ea8a3afc
User <l.sedighi.2015@example.com>
2019-10-23 16:09:25 +0330

    Init project

---
+++ app/src/main/java/com/example/homework12/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.homework12;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+    }
+}
