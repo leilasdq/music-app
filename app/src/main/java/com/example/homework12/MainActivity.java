21cf8f7cd8c7b4fd6810e6646751cf9d3f82e904
User <l.sedighi.2015@example.com>
2019-10-23 18:57:14 +0330

    Add a BottomNavigationView to mainActivity to show fragments as user wants.

--- app/src/main/java/com/example/homework12/MainActivity.java
+++ app/src/main/java/com/example/homework12/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.homework12;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar mToolbar;
+    BottomNavigationView mBottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +21,28 @@ public class MainActivity extends AppCompatActivity {
 
         mToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+
+        mBottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, MusicFragment.newInstance()).commit();
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment = null;
+
+                switch (menuItem.getItemId()){
+                    case R.id.bnm_music:
+                        selectedFragment = MusicFragment.newInstance();
+                        break;
+                    case R.id.bnm_album:
+                        selectedFragment = AlbumFragment.newInstance();
+                        break;
+                    case R.id.bnm_singer:
+                        selectedFragment = SingerFragment.newInstance();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selectedFragment).commit();
+                return true;
+            }
+        });
     }
 }
